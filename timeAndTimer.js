fe2a62497bc52c80bcb3acbd1c2bd96f444b3dcb
Yiwei Shen <YiWeiShen@users.noreply.github.com>
2022-02-18 17:44:02 -0500

    save

--- timeAndTimer.js
+++ timeAndTimer.js
@@ -1,13 +1,25 @@
+var startTime = new Date().getTime();
+
 setInterval(function() {
-    var now = new Date().getTime();
-    var distance = now - store.startTime;
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var date = new Date();
+    var timeDiff = date.getTime() - startTime;
+
+    var h = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((timeDiff % (1000 * 60)) / 1000);
   
-    hours = (hours < 10) ? ('0' + hours) : hours;
-    minutes = (minutes < 10) ? ('0' + minutes) : minutes;
-    seconds = (seconds < 10) ? ('0' + seconds) : seconds;
+    var timer_h = (h < 10) ? ('0' + h) : h;
+    var timer_m = (m < 10) ? ('0' + m) : m;
+    var timer_s = (s < 10) ? ('0' + s) : s;
   
-    $('#timer').html('Timer: ' + hours + ':' + minutes + ':' + seconds);
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+
+    var time_h = (h < 10) ? ('0' + h) : h;
+    var time_m = (m < 10) ? ('0' + m) : m;
+    var time_s = (s < 10) ? ('0' + s) : s;
+
+    $('#time').html('Time: ' + time_h + ':' + time_m + ':' + time_s);
+    $('#timer').html('Timer: ' + timer_h + ':' + timer_m + ':' + timer_s);
   }, 1000);
